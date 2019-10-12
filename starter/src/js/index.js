1d66ec01aa5296decbca0f0be2ab3bc12ce5d004
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-10-12 22:52:36 +0100

    Import and Export between js MVC

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,6 +1,12 @@
 // Global app controller
-import num from './test';
 
-const x = 23;
+import string from './models/Search';
+// 1st WAY
+//import {add as a, mult as m, ID} from './views/searchView';
 
-console.log(`I imported ${num} from another module testing! 12345 Variable x is ${x}`);
+import * as searchView from './views/searchView';
+
+// 1st WAY
+//console.log(`Using imported functions ${a(ID, 2)} and ${m(3,5)}. ${string} `);
+
+console.log(`Using imported functions ${searchView.add(searchView.ID, 2)} and ${searchView.mult(3,5)}. ${string} `);
