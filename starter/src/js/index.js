6ef5d58049cb6a8697a2f4624cfd8093eb00a61f
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-10-14 20:22:46 +0100

    Base.js, Index.js (controller), SearchView - IMPORTANT

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,6 +1,46 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('Pizza');
-console.log(search)
+/**
+ * GLOBAL STATE OF THE APP
+ * SEARCH OBJECT
+ * CURRENT RECIPE OBJECT
+ * SHOPPING LIST OBJECT
+ * LIKED RECIPES
+ */
 
-search.getResult();
+const state = {}; 
+
+const controlSearch = async() => {
+
+    // 1.- GET THE QUERY FROM THE VIEW
+    const query = searchView.getInput();
+    
+    if (query) {
+        // 2.- NEW SEARCH OBJECT AND ADD STATE
+        state.search = new Search(query);
+
+        // 3.- PREPARE UI FOR RESULT
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4.- SEARCH FOR RECIPES
+        await state.search.getResult();
+
+        // 5.- RENDER RESULT ON UI
+        searchView.renderResults(state.search.result);
+    }
+
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+//const search = new Search('Pizza');
+//console.log(search)
+
+//search.getResult();
