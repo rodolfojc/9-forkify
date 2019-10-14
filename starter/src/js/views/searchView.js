968c9d87e523d709b77c3a0438aa8f33c59452f3
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-10-14 20:43:12 +0100

    Reduce the Title length - algorithm

--- starter/src/js/views/searchView.js
+++ starter/src/js/views/searchView.js
@@ -10,6 +10,32 @@ export const clearResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+// PASTA WITH TOMATO AND SPINACH
+/**
+ * acc = 0 / acc + cur.length = 5 / newTitle = ['PASTA'];
+ * acc = 5 / acc + cur.length = 9 / newTitle = ['PASTA', 'WITH'];
+ * acc = 9 / acc + cur.length = 15 / newTitle = ['PASTA', 'WITH', 'TOMATO'];
+ * acc = 15 / acc + cur.length = 18 / newTitle = ['PASTA', 'WITH', 'TOMATO']; // NO PASS
+ * acc = 18 / acc + cur.length = 24 / newTitle = ['PASTA', 'WITH', 'TOMATO']; // NO PASS *  
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+
+        return `${newTitle.join(' ')} ...`;
+    }
+
+    return title;
+    
+};
+
 const renderRecipe = recipe => {
 
     const markup = `
@@ -19,7 +45,7 @@ const renderRecipe = recipe => {
                             <img src="${recipe.image_url}" alt="${recipe.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipe.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                             <p class="results__author">${recipe.publisher}</p>
                         </div>
                     </a>
