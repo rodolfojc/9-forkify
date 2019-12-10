301dc9209980f5d90fabce8a4a063202e3b5c3de
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-12-10 23:55:04 +0000

    Adding Items to Shopping List

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 
@@ -122,6 +123,27 @@ elements.searchResPages.addEventListener('click', e =>{
 
  //['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+ /*
+ * LIST CONTROLLER
+ *
+ */
+
+const controlList = () => {
+
+    // CREATE A NEW LIST IF THERE IN NONE YET
+    if (!state.list) state.list = new List();
+
+    // ADD EACH INGREDIENT TO THE LIST
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+
+
+
+}
+
+
  // HANDELING RECIPE BUTTON CLICKS
  elements.recipe.addEventListener('click', e => {
      if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -135,7 +157,9 @@ elements.searchResPages.addEventListener('click', e =>{
         // INCREASE IS CLICK
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
-     }     
+     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        controlList();
+     }
  });
 
  window.l = new List();
